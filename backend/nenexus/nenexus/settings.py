adf68cf5e407958f4f5015ad5b1a5e71a29ea2eb
Danila Olyanin <danilaolyanin@yandex.ru>
2025-05-21 20:29:59 +0300

    Configure CORS

--- backend/nenexus/nenexus/settings.py
+++ backend/nenexus/nenexus/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_filters',
     'djoser',
     'package.apps.PackageConfig',
@@ -52,12 +53,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 ROOT_URLCONF = 'nenexus.urls'
 
